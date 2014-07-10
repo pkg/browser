da95f49dbaf647970bb88b378237f938b51aaea6
Dave Cheney <dave@cheney.net>
2014-07-10 17:53:48 +1000

    Fix open on linux

--- browser_linux.go
+++ browser_linux.go
@@ -1,14 +1,22 @@
 package browser
 
 import (
+	"os"
 	"os/exec"
 )
 
 func openBrowser(url string) error {
-	sensibleBrowser, err := exec.LookPath("sensible-browser")
-	if err != nil {
-		// sensible-browser not availble, try xdg-open
-		return exec.Command("xdg-open", url).Run()
+	// try sensible-browser first
+	if err := runCmd("sensible-browser", url); err == nil {
+		return nil
 	}
-	return exec.Command(sensibleBrowser, url).Run()
+	// sensible-browser not availble, try xdg-open
+	return runCmd("xdg-open", url)
+}
+
+func runCmd(prog string, args ...string) error {
+	cmd := exec.Command(prog, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
