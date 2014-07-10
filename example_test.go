63953a810e5be733f37841ef23288d3ed225522e
Dave Cheney <dave@cheney.net>
2014-07-10 17:23:36 +1000

    fix quote layout

--- example_test.go
+++ example_test.go
@@ -8,11 +8,11 @@ func ExampleOpenFile() {
 
 func ExampleOpenReader() {
 	// https://github.com/rust-lang/rust/issues/13871
-	const quote = `There was a night when winds from unknown spaces whirled us irresistibly into
-limitless vacum beyond all thought and entity. Perceptions of the most
-maddeningly untransmissible sort thronged upon us; perceptions of infinity
-which at the time convulsed us with joy, yet which are now partly lost to my
-memory and partly incapable of presentation to others.`
+	const quote = `There was a night when winds from unknown spaces
+whirled us irresistibly into limitless vacum beyond all thought and entity.
+Perceptions of the most maddeningly untransmissible sort thronged upon us;
+perceptions of infinity which at the time convulsed us with joy, yet which
+are now partly lost to my memory and partly incapable of presentation to others.`
 	r := strings.NewReader(quote)
 	OpenReader(r)
 }
