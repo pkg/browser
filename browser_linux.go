647cc3c01339b0b4654a9cadc6a2ff41abd837f6
Henrique Vicente <henriquevicente@gmail.com>
2017-08-23 03:03:33 -0300

    Fixing browser.OpenURL on Windows Subsystem for Linux.

--- browser_linux.go
+++ browser_linux.go
@@ -1,5 +1,22 @@
 package browser
 
+import (
+	"os/exec"
+	"strings"
+)
+
+func hasProgram(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
+}
+
 func openBrowser(url string) error {
+	// Windows Subsystem for Linux (bash for Windows) doesn't have xdg-open available
+	// but you can execute cmd.exe from there; try to identify it
+	if !hasProgram("xdg-open") && hasProgram("cmd.exe") {
+		r := strings.NewReplacer("&", "^&")
+		return runCmd("cmd.exe", "/c", "start", r.Replace(url))
+	}
+
 	return runCmd("xdg-open", url)
 }
