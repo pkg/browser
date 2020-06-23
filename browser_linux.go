05cb0ec9af669ef96d82176bf48bf4679099df5e
Pete Emerson <pemerson@standard.ai>
2020-06-23 15:46:13 -0700

    Fix xdg-open behavior under Linux

--- browser_linux.go
+++ browser_linux.go
@@ -1,9 +1,34 @@
 package browser
 
-import "os/exec"
+import (
+	"os/exec"
+	"time"
+)
+
+func appearsSuccessful(cmd *exec.Cmd, timeout time.Duration) bool {
+	errc := make(chan error, 1)
+	go func() {
+		errc <- cmd.Wait()
+	}()
+
+	select {
+	case <-time.After(timeout):
+		return true
+	case err := <-errc:
+		return err == nil
+	}
+}
 
 func openBrowser(url string) error {
-	return runCmd("xdg-open", url)
+	cmd := exec.Command("xdg-open", url)
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	if !appearsSuccessful(cmd, 3*time.Second) {
+		return err.New("Unable to successfully run xdg-open")
+	}
+	return nil
 }
 
 func setFlags(cmd *exec.Cmd) {}
