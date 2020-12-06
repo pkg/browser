2d6020680f0a451e99d192af09fdae2d692aa07c
Mislav Marohnić <git@mislav.net>
2020-12-06 13:46:10 +0100

    Add WSL fallback for Linux

--- browser_linux.go
+++ browser_linux.go
@@ -1,9 +1,26 @@
 package browser
 
-import "os/exec"
+import (
+	"errors"
+	"os/exec"
+)
 
 func openBrowser(url string) error {
-	return runCmd("xdg-open", url)
+	browserPath, err := lookPath("xdg-open", "wslview")
+	if err != nil {
+		return err
+	}
+	return runCmd(browserPath, url)
+}
+
+func lookPath(cmdName, fallbackName string) (string, error) {
+	cmdPath, err := exec.LookPath(cmdName)
+	if errors.Is(err, exec.ErrNotFound) {
+		if wslPath, err := exec.LookPath(fallbackName); err == nil {
+			return wslPath, nil
+		}
+	}
+	return cmdPath, err
 }
 
 func setFlags(cmd *exec.Cmd) {}
