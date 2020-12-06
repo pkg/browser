fa777652a9637840322dc8783995e6e41a7623f0
Mislav Marohnić <git@mislav.net>
2020-12-06 13:45:12 +0100

    Use safeexec on Windows

--- browser_windows.go
+++ browser_windows.go
@@ -4,11 +4,17 @@ import (
 	"os/exec"
 	"strings"
 	"syscall"
+
+	"github.com/cli/safeexec"
 )
 
 func openBrowser(url string) error {
+	cmdPath, err := safeexec.LookPath("cmd")
+	if err != nil {
+		return err
+	}
 	r := strings.NewReplacer("&", "^&")
-	return runCmd("cmd", "/c", "start", r.Replace(url))
+	return runCmd(cmdPath, "/c", "start", r.Replace(url))
 }
 
 func setFlags(cmd *exec.Cmd) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cli/browser
 
 go 1.13
+
+require github.com/cli/safeexec v1.0.0
