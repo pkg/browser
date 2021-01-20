5755fc38a3b1757aa355bf4faf5d26b34dcd298f
Dave Cheney <davecheney@github.com>
2021-01-21 10:15:52 +1100

    Revert "Windows: utilize ShellExecuteW"

--- browser_windows.go
+++ browser_windows.go
@@ -1,13 +1,16 @@
-//go:generate mkwinsyscall -output zbrowser_windows.go browser_windows.go
-//sys ShellExecute(hwnd int, verb string, file string, args string, cwd string, showCmd int) (err error) = shell32.ShellExecuteW
 package browser
 
-import "os/exec"
-const SW_SHOWNORMAL = 1
+import (
+	"os/exec"
+	"strings"
+	"syscall"
+)
 
 func openBrowser(url string) error {
-   return ShellExecute(0, "", url, "", "", SW_SHOWNORMAL)
+	r := strings.NewReplacer("&", "^&")
+	return runCmd("cmd", "/c", "start", r.Replace(url))
 }
 
 func setFlags(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 }
